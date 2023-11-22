16bdea8163575e94448fdee2b4ad56f058be96b9
Lily <hayashi.quan@gmail.com>
2023-11-22 18:22:04 +0100

    fix - move functions in other file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { resetServiceWorker } from '@/utils/service-worker';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -60,18 +61,4 @@ const subscribe = async () => {
     } catch (err) {
         console.error('Error', err);
     }
-};
-
-export const registerServiceWorker = async () => {
-    return navigator.serviceWorker.register('/service.js');
-};
-
-export const unregisterServiceWorkers = async () => {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    await Promise.all(registrations.map((r) => r.unregister()));
-};
-
-export const resetServiceWorker = async () => {
-    await unregisterServiceWorkers();
-    return registerServiceWorker();
 };
