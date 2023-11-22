fee2789ca744412dc22e96c91c0b1f953536b1fd
Lily <hayashi.quan@gmail.com>
2023-11-22 18:07:19 +0100

    add notif stuff

---
+++ app/api/push/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { getSubscriptionsFromDb, saveSubscriptionToDb } from '@/utils/db/in-memory-db';
+import webpush, { PushSubscription } from 'web-push';
+
+webpush.setVapidDetails('mailto:hayashi.quan@gmail.com', process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!, process.env.VAPID_PRIVATE_KEY!);
+
+export async function POST(request: NextRequest) {
+    const subscription = (await request.json()) as PushSubscription | null;
+
+    if (!subscription) {
+        console.error('No subscription was provided!');
+        return;
+    }
+
+    const updatedDb = await saveSubscriptionToDb(subscription);
+
+    return NextResponse.json({ message: 'success', updatedDb });
+}
+
+export async function GET(_: NextRequest) {
+    const subscriptions = await getSubscriptionsFromDb();
+
+    subscriptions.forEach((s) => {
+        const payload = JSON.stringify({
+            title: 'WebPush Notification!',
+            body: 'Hello World',
+        });
+        webpush
+            .sendNotification(s, payload)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    });
+
+    return NextResponse.json({
+        message: `${subscriptions.length} messages sent!`,
+    });
+}
---
+++ environment.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NEXT_PUBLIC_VAPID_PUBLIC_KEY: string;
+            VAPID_PRIVATE_KEY: string;
+        }
+    }
+}
