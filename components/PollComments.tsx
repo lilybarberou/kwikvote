3febb9e44661588f173d72f5647acf13e68c61ed
Lily <hayashi.quan@gmail.com>
2023-11-26 23:04:03 +0100

    update comments design

--- components/PollComments.tsx
+++ components/PollComments.tsx
@@ -1,6 +1,8 @@
-import { Comment } from '@prisma/client';
 import { useState } from 'react';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
 import { useForm } from 'react-hook-form';
+import { Comment } from '@prisma/client';
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
@@ -20,7 +22,7 @@ export default function PollComments(props: Props) {
     const [comments, setComments] = useState<Comment[]>(props.comments);
     const { register, handleSubmit, reset } = useForm<CommentForm>();
 
-    // add a comment
+    // ADD A COMMENT
     const onCommentSubmit = handleSubmit(async (data) => {
         const res = await fetch('/api/comment', {
             method: 'POST',
@@ -34,22 +36,21 @@ export default function PollComments(props: Props) {
     });
 
     return (
-        <div>
-            <p>Ajouter un commentaire</p>
-            <form onSubmit={onCommentSubmit} className="w-72 flex flex-col gap-2">
-                <Input placeholder="Auteur" {...register('author', { required: true })} />
+        <div className="mt-6 flex flex-col gap-2">
+            {comments.map((comment, index) => (
+                <div key={comment.id} className={index < comments.length - 1 ? 'pb-3 border-b' : ''}>
+                    <p>
+                        {comment.author}{' '}
+                        <span className="ml-1 text-xs text-muted-foreground">{format(new Date(comment.createdAt), 'PPp', { locale: fr })}</span>
+                    </p>
+                    <p>{comment.text}</p>
+                </div>
+            ))}
+            <form onSubmit={onCommentSubmit} className="mt-5 p-2 w-[350px] flex flex-col gap-2 rounded bg-muted">
+                <Input className="w-[230px]" placeholder="Auteur" {...register('author', { required: true })} />
                 <Textarea placeholder="Message" {...register('text', { required: true })} />
                 <Button>Envoyer</Button>
             </form>
-            <div className="flex flex-col gap-2">
-                <p>Liste des commentaires</p>
-                {comments.map((comment) => (
-                    <div key={comment.id} className="border">
-                        <p>{comment.author}</p>
-                        <p>{comment.text}</p>
-                    </div>
-                ))}
-            </div>
         </div>
     );
 }
