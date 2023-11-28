c90aa957632ad60a986cdeacd41ff2e6799d4dc8
Lily <hayashi.quan@gmail.com>
2023-11-28 09:49:39 +0100

    add error state for poll page

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -6,10 +6,11 @@ import PollSlots from '@/components/PollSlots';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import { useVotesStore } from '@/lib/votesStore';
 import fetcher from '@/utils/fetch';
-import { Megaphone } from 'lucide-react';
+import { BarChart3, Megaphone } from 'lucide-react';
 import useSWR from 'swr';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import PollSkeleton from '@/components/PollSkeleton';
+import Link from 'next/link';
 
 export default function PollPage({ params }: { params: { id: string } }) {
     const { initVotes } = useVotesStore();
@@ -24,9 +25,22 @@ export default function PollPage({ params }: { params: { id: string } }) {
     });
 
     if (isLoading) return <PollSkeleton />;
-    if (error || !poll) return <div>Ce sondage n&apos;existe pas</div>;
+    if (error || !poll)
+        return (
+            <div className="mx-auto mt-20 h-full flex flex-col justify-center items-center">
+                <BarChart3 className="mb-10 w-24 h-24" />
+                <p className="text-2xl font-bold">Ce sondage n&apos;existe pas</p>
+                <p className="text-muted-foreground">
+                    Vous pouvez créer un sondage via la{' '}
+                    <Link className="text-primary" href="/poll/create">
+                        page de création
+                    </Link>
+                    .
+                </p>
+            </div>
+        );
     return (
-        <div className="m-auto mt-4">
+        <div className="mt-4">
             <h1 className="mb-5 text-lg">{poll?.title}</h1>
             {poll.description && (
                 <Alert className="w-fit">
@@ -34,7 +48,7 @@ export default function PollPage({ params }: { params: { id: string } }) {
                     <AlertDescription>{poll.description}</AlertDescription>
                 </Alert>
             )}
-            <Tabs defaultValue="votes" className="mt-10 w-fit">
+            <Tabs defaultValue="votes" className="mt-10">
                 <TabsList>
                     <TabsTrigger value="votes">Votes</TabsTrigger>
                     <TabsTrigger value="comments">Commentaires ({poll.comments.length})</TabsTrigger>
