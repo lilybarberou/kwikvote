03175df5f96844b2d0a8ddc1e9788281117a5f25
Lily <hayashi.quan@gmail.com>
2023-11-29 11:46:06 +0100

    update service notif click event

--- public/service.js
+++ public/service.js
@@ -8,18 +8,30 @@ self.addEventListener('push', async (event) => {
 const showLocalNotification = (title, body, link, swRegistration) => {
     swRegistration.showNotification(title, {
         body,
-        actions: [{ action: link, title: 'Lien du sondage' }],
         icon: '/icons/icon-192.png',
+        tag: link,
     });
 };
 
-self.addEventListener('notificationclick', (event) => {
-    // Get the URL to navigate to (in this case, '/home')
-    const urlToOpen = new URL(event.action).href;
-
-    // Prevent other listeners from handling the event
+self.addEventListener('notificationclick', function (event) {
+    // Android doesn't close the notification when you click on it
     event.notification.close();
 
-    // Open a new window/tab with the specified URL
-    event.waitUntil(clients.openWindow(urlToOpen));
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+        clients
+            .matchAll({
+                type: 'window',
+            })
+            .then(function (clientList) {
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
+                    if (client.url == '/' && 'focus' in client) return client.focus();
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(event.notification.tag);
+                }
+            })
+    );
 });
