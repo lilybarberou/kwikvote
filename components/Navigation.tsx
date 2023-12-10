75c3df9d25097342cf82b2f8c40df72fd7b46fa1
Lily <hayashi.quan@gmail.com>
2023-12-10 17:04:16 +0100

    remove nav tooltip on mobile

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { HelpCircle, Home } from 'lucide-react';
+import { HelpCircle, Home, Sparkles } from 'lucide-react';
 import { Button } from './ui/button';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 
@@ -15,12 +15,12 @@ export default function Navigation() {
                             </Button>
                         </TooltipTrigger>
                     </Link>
-                    <TooltipContent>Accueil</TooltipContent>
+                    <TooltipContent className="hidden sm:block">Accueil</TooltipContent>
                 </Tooltip>
             </TooltipProvider>
             <div className="flex gap-2">
                 <Link href="/poll/create" legacyBehavior>
-                    <Button className="">Créer un sondage</Button>
+                    <Button className="h-11">Créer un sondage</Button>
                 </Link>
                 <TooltipProvider>
                     <Tooltip delayDuration={300}>
@@ -31,9 +31,21 @@ export default function Navigation() {
                                 </Button>
                             </TooltipTrigger>
                         </Link>
-                        <TooltipContent>FAQ</TooltipContent>
+                        <TooltipContent className="hidden sm:block">FAQ</TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
+                {/* <TooltipProvider>
+                    <Tooltip delayDuration={300}>
+                        <Link href="/changelog" legacyBehavior>
+                            <TooltipTrigger asChild>
+                                <Button className="w-11 h-11" size="icon" variant="outline">
+                                    <Sparkles className="w-5 h-5" />
+                                </Button>
+                            </TooltipTrigger>
+                        </Link>
+                        <TooltipContent>Changelog</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider> */}
             </div>
         </nav>
     );
