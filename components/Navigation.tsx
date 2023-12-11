22d973a3035371373d63f7ce3aa9f91e3b1f9819
Lily <hayashi.quan@gmail.com>
2023-12-11 19:57:44 +0100

    fix navigation accessibility (right click for link etc)

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { HelpCircle, Home, Sparkles } from 'lucide-react';
+import { HelpCircle, Home } from 'lucide-react';
 import { Button } from './ui/button';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 
@@ -8,44 +8,32 @@ export default function Navigation() {
         <nav className="mb-10 flex justify-between">
             <TooltipProvider>
                 <Tooltip delayDuration={300}>
-                    <Link href="/" legacyBehavior>
-                        <TooltipTrigger asChild>
-                            <Button className="w-11 h-11" size="icon" variant="outline">
+                    <TooltipTrigger asChild>
+                        <Button asChild className="w-11 h-11" size="icon" variant="outline">
+                            <Link href="/">
                                 <Home className="w-5 h-5" />
-                            </Button>
-                        </TooltipTrigger>
-                    </Link>
+                            </Link>
+                        </Button>
+                    </TooltipTrigger>
                     <TooltipContent className="hidden sm:block">Accueil</TooltipContent>
                 </Tooltip>
             </TooltipProvider>
             <div className="flex gap-2">
-                <Link href="/poll/create" legacyBehavior>
-                    <Button className="h-11">Créer un sondage</Button>
-                </Link>
+                <Button asChild className="h-11">
+                    <Link href="/poll/create">Créer un sondage</Link>
+                </Button>
                 <TooltipProvider>
                     <Tooltip delayDuration={300}>
-                        <Link href="/faq" legacyBehavior>
-                            <TooltipTrigger asChild>
-                                <Button className="w-11 h-11" size="icon" variant="outline">
+                        <TooltipTrigger asChild>
+                            <Button asChild className="w-11 h-11" size="icon" variant="outline">
+                                <Link href="/faq">
                                     <HelpCircle className="w-5 h-5" />
-                                </Button>
-                            </TooltipTrigger>
-                        </Link>
+                                </Link>
+                            </Button>
+                        </TooltipTrigger>
                         <TooltipContent className="hidden sm:block">FAQ</TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
-                {/* <TooltipProvider>
-                    <Tooltip delayDuration={300}>
-                        <Link href="/changelog" legacyBehavior>
-                            <TooltipTrigger asChild>
-                                <Button className="w-11 h-11" size="icon" variant="outline">
-                                    <Sparkles className="w-5 h-5" />
-                                </Button>
-                            </TooltipTrigger>
-                        </Link>
-                        <TooltipContent>Changelog</TooltipContent>
-                    </Tooltip>
-                </TooltipProvider> */}
             </div>
         </nav>
     );
