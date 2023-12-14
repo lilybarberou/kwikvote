3400fce98da1006eb00c059fe817d89c72a4bf2a
Lily <hayashi.quan@gmail.com>
2023-12-14 11:28:05 +0100

    update nav design

--- components/ConsultedHistory.tsx
+++ components/ConsultedHistory.tsx
@@ -6,11 +6,11 @@ export default function ConsultedHistory() {
     const { pollHistory } = useHistoryStore();
 
     return (
-        <div className="w-full min-w-[200px] flex flex-col gap-1">
+        <div className="w-full min-w-[200px] flex flex-col">
             {pollHistory.map((poll, index) => (
                 <PopoverClose key={poll.pollId} asChild>
                     <Link
-                        className={`relative py-2 px-3 text-ellipsis overflow-hidden ${
+                        className={`relative py-2 px-3 text-sm text-ellipsis overflow-hidden ${
                             index === pollHistory.length - 1
                                 ? ''
                                 : 'after:absolute after:bottom-0 after:left-3 after:h-1 after:w-[calc(100%-24px)] after:border-b after:border-[#ffffff29]'
