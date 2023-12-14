b276ddf95d7f5f94ad12f886cb49bb495b0ed529
Lily <hayashi.quan@gmail.com>
2023-12-14 15:27:29 +0100

    fix copy to clipboard on mobile

--- components/DialogPollLink.tsx
+++ components/DialogPollLink.tsx
@@ -11,8 +11,15 @@ export default function DialogPollLink() {
     const { toast } = useToast();
 
     const copyPollLink = () => {
-        navigator.clipboard.writeText(window.location.href.split('?')[0]);
-        toast({ title: 'Lien copié', description: 'Vous pouvez maintenant partager votre sondage.' });
+        if (navigator?.clipboard?.writeText) {
+            navigator.clipboard.writeText(window.location.href.split('?')[0]);
+            toast({ title: 'Lien copié', description: 'Vous pouvez maintenant partager votre sondage.' });
+        } else {
+            toast({
+                title: 'Woups, échec de la copie sur votre appareil',
+                description: 'Pas de panique ! Vous pouvez copier le lien manuellement en restant appuyé dessus (si vous êtes sur mobile).',
+            });
+        }
     };
 
     const removeQueryParams = () => router.push(window.location.href.split('?')[0]);
@@ -24,10 +31,10 @@ export default function DialogPollLink() {
                 <AlertDialogDescription>
                     <p className="mb-2">Conservez bien votre lien :</p>
                     <div className="w-fit flex gap-2 items-center cursor-pointer" onClick={copyPollLink}>
-                        <p className="py-1 px-2 max-w-[200px] border border-[#ffffff4d] rounded text-ellipsis overflow-hidden whitespace-nowrap">
+                        <p className="py-1 px-2 max-w-[250px] border border-[#ffffff4d] rounded text-ellipsis overflow-hidden whitespace-nowrap md:max-w-[200px]">
                             {window.location.href.split('?')[0]}
                         </p>
-                        <Button className="px-2 h-6" tabIndex={1} onClick={copyPollLink} variant="ghost">
+                        <Button className="px-2 h-fit py-2 hidden md:block" tabIndex={1} onClick={copyPollLink} variant="ghost">
                             <Copy className="w-5 h-5" />
                         </Button>
                     </div>
